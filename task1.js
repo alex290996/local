893fe360918ea5c8e17daa387f1015b449b03f57
alex290996 <40002454+alex290996@users.noreply.github.com>
2018-06-15 08:02:48 +0500

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,54 +1,20 @@
 
-var userChoice = proccess.argv[2];
-var computerChoice = Math.floor(Math.random() * 100);
-if (proccess.arr[2].length === 1) {
-    userChoice = proccess.arr[2] * 10;
-} else if(proccess.arr[2].length === 2) {
-    userChoice = proccess.arr[2];
-} else {
-    userChoice = proccess.arr[2] / 10;
-}
-if (computerChoice < 34) {
-    computerChoice = "камень";
-} else if(computerChoice <= 67) {
-    computerChoice = "бумага";
-} else {
-    computerChoice = "ножницы";
-}
-if (userChoice < 34) {
-    userChoice = "камень";
-} else if(userChoice <= 67) {
-    userChoice = "бумага";
-} else {
-    userChoice = "ножницы";
-}
-process.stdout.write("Выбор пользователя: " + userChoice);
-process.stdout.write("Выбор компьютера: " + computerChoice);
-compare(userChoice, computerChoice);
-var compare = function (choice1, choice2)
-{if (choice1 === choice2)
-{ process.stdout.write("Ничья!");
-} else if (choice1 === "камень") {
-    if (choice2 === "ножницы") {
-        process.stdout.write("Вы выиграли.");
+function Palindrome(str) {
+    var strLen = str.length;
+    var result = '';
+    for (var i = 0; i < strLen; i++) {
+      if (str[i] === str[strLen - 1 - i]) { 
+        result = 'yes';
+      } else {
+        result = 'no';
+        return result;
+      }
     }
-    else if (choice2 === "бумага") {
-        process.stdout.write("Вы проиграли.");
-    }
-} else if (choice1 === "бумага") {
-    if (choice2 === "камень") {
-        process.stdout.write("Вы выиграли.");
-    }
-    else if (choice2 === "ножницы") {
-        process.stdout.write("Вы проиграли.");
-    }
-} else if (choice1 === "ножницы") {
-    if (choice2 === "камень") {
-        process.stdout.write("Вы проиграли.");
-    }
-    else if (choice2 === "бумага") {
-        process.stdout.write("Вы выиграли.");
-    }
-} 
-}
+    return result;
+  }
 
+  test = Palindrome(process.argv[2]);
+  test2 = Palindrome(process.argv[3]);
+
+  process.stdout.write(test);
+  process.stdout.write(test2);
