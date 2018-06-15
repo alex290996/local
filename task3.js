05689faccc73c9505aaa2b3b1b2e607d477a2462
alex290996 <40002454+alex290996@users.noreply.github.com>
2018-06-15 08:10:10 +0500

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,11 +1,45 @@
-function compareNumbers(a, b) {
-  return a - b;
+function word(num, words)
+{
+	num = num%100;
+	if (num > 20)
+		num %= 10;
+	return words[ (num > 4 || num === 0) + (num !== 1) ];
 }
 
-var myArray = [];
-for (i = 10; i > 0; i--) {
-myArray[i] = process.argv[i];}
+function sumTime(tone, ttwo)
+{	
+		tone = parseInt(tone);
+		ttwo = parseInt(ttwo);
 
-process.stdout.write("array before sorting " + myArray); 
-myArray = myArray.sort(compareNumbers);
-process.stdout.write("array after sorting " + myArray); 
+		const t = tone + ttwo;
+		const h = Math.floor(t/3600);
+		const m = Math.floor((t - 3600*h)/60);
+		const s = t - 3600*h - 60*m;
+
+		const hoursArray = ['час', 'часа', 'часов'];
+		const minutesArray = ['минута', 'минуты', 'минут'];
+		const secondsArray = ['секунда', 'секунды', 'секунд'];
+
+	if ((m === 0) && (s === 0))
+		process.stdout.write(String(h + ' ' + word(h, hoursArray)));
+	else
+		if ((h !== 0) && (s === 0))
+			process.stdout.write(String(h + ' ' + word(h, hoursArray) + ' ' + m + ' ' + word(m, minutesArray)));
+		else
+			if (s === 0)
+				process.stdout.write(String(m + ' ' + word(m, minutesArray)));
+			else
+				if ((h !== 0) && (m !== 0))
+					process.stdout.write(String(h + ' ' + word(h, hoursArray) + ' ' + m + ' ' + word(m, minutesArray) + ' ' + s + ' ' + word(s, secondsArray)));
+				else
+					if (h !== 0)
+						process.stdout.write(String(h + ' ' + word(h, hoursArray) + ' ' + s + ' ' + word(s, secondsArray)));
+					else
+						if (m !== 0)
+							process.stdout.write(String(m + ' ' + word(m, minutesArray) + ' ' + s + ' ' + word(s, secondsArray)));
+						else
+							process.stdout.write(String(s + ' ' + word(s, secondsArray)));
+	
+}
+
+sumTime([process.argv[2], process.argv[3]);
